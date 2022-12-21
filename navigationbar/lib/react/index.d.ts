2816baa199b0c657957fd58c0755d573fb0a86ce
Jordan <jazoulay@joazco.com>
2022-12-21 17:57:44 +0100

    debug test

--- navigationbar/lib/react/index.d.ts
+++ navigationbar/lib/react/index.d.ts
@@ -1,9 +1,3 @@
-/**
- * @author AZOULAY Jordan<jazoulay@joazco.com>
- * This plugin defines a global navigator.camera object, which provides an API for taking pictures and for choosing images from the system's image library.
- * Also, check out {@link https://cordova.apache.org/docs/en/11.x/reference/cordova-plugin-camera/index.html Cordova}
- * @requires module:cordova-plugin-camera
- */
 declare const useNavigationBar: () => {
     setUp: (autoHideNavigationBar: boolean) => Promise<void>;
     hideNavigationBar: () => Promise<void>;
